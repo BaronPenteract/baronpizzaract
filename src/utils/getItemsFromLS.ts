ddff95ed0ff07dc507bffb19cf646a82b773ce5a
Andrey <Andreas42R@yandex.ru>
2023-04-21 16:55:32 +0700

    fix: get items from localStorage

--- src/utils/getItemsFromLS.ts
+++ src/utils/getItemsFromLS.ts
@@ -2,11 +2,19 @@ import { CartItemType } from "../redux/slices/cartSlice";
 
 export const getItemsFromLS = () => {
   const data = localStorage.getItem('cart') || '';
-  const items: CartItemType[] = JSON.parse(data) || [];
-  const totalPrice: number = items.reduce((acc, item) => (acc += item.price * item.count), 0);
+  if(data) {
+    const items: CartItemType[] = JSON.parse(data)
+    const totalPrice: number = items.reduce((acc, item) => (acc += item.price * item.count), 0);
 
-  return {
-    items,
-    totalPrice
+    return {
+      items,
+      totalPrice
+    }
+  } else {
+    return {
+      items: [],
+      totalPrice: 0
+    } as {items: CartItemType[], totalPrice: number}
   }
+
 }
